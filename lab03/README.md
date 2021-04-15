ad731e45a77aef8eb39a019876e297d50a06d5ab
Guilherme <guilhermeg12.goncalves@gmail.com>
2021-04-15 07:23:39 -0300

    Adiciona resolução do lab03

---
+++ lab03/README.md
@@ -0,0 +1,7 @@
+# Lab 03
+
+## Notebook sobre Lombriga no Aquário
+[Notebook](notebook/lab-lombriga-ra170927.ipynb)
+
+## Arquivos Java sobre Lombriga no Aquário
+[Arquivos Java](src/mc322/lab03)
---
+++ lab03/src/mc322/lab03/AppLab03.java
@@ -0,0 +1,9 @@
+package mc322.lab03;
+
+public class AppLab03 {
+	
+    public static void main(String[] args) {
+        Animacao animacao = new Animacao("080403MCMVM");
+        animacao.passo();
+    }
+}
