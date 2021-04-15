06d77c6aa3ddd7819aa479306168ec13f827ed7e
GuilhermeGG12 <61219460+GuilhermeGG12@users.noreply.github.com>
2021-04-15 07:01:15 -0300

    Delete lab03/mc322 directory

--- lab03/mc322/src/mc322/lab03/AppLab03.java
+++
@@ -1,9 +0,0 @@
-package mc322.lab03;
-
-public class AppLab03 {
-	
-    public static void main(String[] args) {
-        Animacao animacao = new Animacao("080403MCMVM");
-        animacao.passo();
-    }
-}
