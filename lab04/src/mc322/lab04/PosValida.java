6c2129dc48eb7586e954c69a8ed00550daa97409
Guilherme <guilhermeg12.goncalves@gmail.com>
2021-04-22 11:41:39 -0300

    Adiciona resolução do lab04

--- lab04/src/mc322/lab04/PosValida.java
+++ lab04/src/mc322/lab04/PosValida.java
@@ -1,6 +1,6 @@
 package mc322.lab04;
 
-public class PosValida {
+public class PosValida { // Verifica se a posição está no tabuleiro
 	public static boolean noTabuleiro(int x, int y) {
 		if((x < 0 || x > 6) || (y < 0 || y > 6))
 			return false;
