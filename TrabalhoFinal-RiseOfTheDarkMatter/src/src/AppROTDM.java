afdedef43e8d5d92601c3ba394fe8a7202b96811
Guilherme <g170927@dac.unicamp.br>
2021-06-17 23:53:45 -0300

    Atualiza valores de width e height do Launcher

--- TrabalhoFinal-RiseOfTheDarkMatter/src/src/AppROTDM.java
+++ TrabalhoFinal-RiseOfTheDarkMatter/src/src/AppROTDM.java
@@ -3,9 +3,14 @@ package src;
 import src.game.Game;
 import src.game.GameLoop;
 
+
 public class AppROTDM {
 
 	public static void main(String[] args) {
-		new Thread(new GameLoop(new Game(800, 600))).start();
+		new Thread(new GameLoop(new Game(16*18*3, 16*17*3))).start();
 	}
 }
+	
+
+
+
